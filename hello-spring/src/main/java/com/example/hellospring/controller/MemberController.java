60d643496ccfca0f54309eae9616f157b713ed0c
Kyeil <meme951023@gmail.com>
2023-01-22 13:18:37 +0900

    간단한 회원 관리 예제 - 웹 MVC 개발  - 회원 가입 홈 기능, 회원 등록, 회원 목록 조회

--- hello-spring/src/main/java/com/example/hellospring/controller/MemberController.java
+++ hello-spring/src/main/java/com/example/hellospring/controller/MemberController.java
@@ -1,26 +1,46 @@
 package com.example.hellospring.controller;
 
+import com.example.hellospring.domain.Member;
 import com.example.hellospring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
+
 
 @Controller
 public class MemberController {
 
-//    @Autowired private MemberService memberService;   // 필드 주입 단점은 뭔가 바꿔줄 수가 없다.
-
-//    private MemberService memberService;
-
-        private final MemberService memberService;
-
-//    @Autowired      // setter 의존성 주입 단점은 누군가가 멤버 컨트롤을 호출했을 때 public으로 열려있어야 한다.
-//                    // 이러면 어느 누구든 접근을 할 수 있게 되어 데이터가 바뀔 위험이 커진다.
-//    public void setMemberService(MemberService memberService){
-//        this.memberService = memberService;
-//    }
+    private MemberService memberService;
 
     @Autowired        // 생성자를 통한 DI
     public MemberController(MemberService memberService) {
         this.memberService = memberService;
+    }
+
+    @GetMapping("/members/new")
+    public String createForm(){
+        return "members/createMemberForm";
+    }
+
+    @PostMapping("members/new")
+    public String create(MemberForm form){
+        Member member = new Member();
+        member.setName(form.getName());
+
+
+        memberService.join(member);
+
+        return "redirect:/";
+    }
+
+    @GetMapping("/members")
+    public String list(Model model){
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members);
+        return "members/memberList";
     }
 }
